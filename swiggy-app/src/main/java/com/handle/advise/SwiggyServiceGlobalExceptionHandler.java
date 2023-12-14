fbceba5b561386d3d5246a81768f8ae81f3b21e6
fabrice78912 <fabricenokam@gmail.com>
2023-12-14 17:54:38 -0500

    update service swiggy

--- swiggy-app/src/main/java/com/handle/advise/SwiggyServiceGlobalExceptionHandler.java
+++ swiggy-app/src/main/java/com/handle/advise/SwiggyServiceGlobalExceptionHandler.java
@@ -1,14 +1,12 @@
-package org.example.common.advice;
+package com.handle.advise;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.example.common.dto.CustomErrorResponse;
 import org.example.common.exception.SwiggyServiceException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
